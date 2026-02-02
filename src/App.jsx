e4505909899fd7ad42fcb132a824da87d3e66f01
Zhanarys <62060069+Jonerbay@users.noreply.github.com>
2026-02-02 08:13:26 +0500

    [ADD] app changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const phrases = [
   "Нет 🙉",
   "Ты уверена?",
   "Точно уверена😢",
-  "Pretty Please🥺",
+  "Плиз плиз плиз🥺",
   "Я буду очень расстроен😕",
   "Я буду очень очень расстроен🫤",
   "Я буду очень очень очень очень расстроен🫤",
@@ -19,12 +19,18 @@ function App() {
   const [yesPressed, setYesPressed] = useState(false);
   const yesButtonSize = noCount * 20 + 16;
 
-  
+  // Edit your details here!
+  const eventDetails = {
+    date: "14-15 Февраля, 2026",
+    location: "Боровое",
+    dressCode: "Твоя самая красивая улыбка 😊"
+  };
+
   function handleYesClick() {
     setYesPressed(true)
   }
   function handleNoClick() {
-    setNoCount(noCount+1);
+    setNoCount(noCount + 1);
   }
 
   function getNoButtonText() {
@@ -34,24 +40,31 @@ function App() {
   return (
     <div className='valentine-container'>
       {yesPressed ? (
-        <>
+        <div className="success-view">
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" alt="bear-Kissing" />
-          <div className='text'>Юхууууу !!!</div>
-        </>
+          <div className='text big'>Юхууууу !!! 🎉</div>
+
+          {/* New Details Section */}
+          <div className="details-card">
+            <h3>До встречи на свидании!</h3>
+            <p>📅 <strong>Дата:</strong> {eventDetails.date}</p>
+            <p>📍 <strong>Место:</strong> {eventDetails.location}</p>
+            <p>👗 <strong>Дресс-код:</strong> {eventDetails.dressCode}</p>
+          </div>
+        </div>
       ) : (
         <>
           <img className='kiss' src="https://media1.tenor.com/m/al4a1pG1f8YAAAAC/jump-bear.gif" alt="bear with heart" />
           <div className='text'>Будешь ли моей валентинкой 🌹?</div>
           <div className='both-Button'>
             <button className='yesButton'
-            style={{ fontSize: yesButtonSize , backgroundColor:'rgb(248, 229, 89)'}}
+              style={{ fontSize: yesButtonSize, backgroundColor: 'rgb(248, 229, 89)' }}
               onClick={handleYesClick}
             >
               Да 🙈
             </button>
             <button
               className='NoButton'
-              style={{ }}
               onClick={handleNoClick}
             >
               {getNoButtonText()}
